10ba4445f23893475eda6913a341ff91f92afeb6
chrisJohns0n <126682002+ChrisJohnson0110@users.noreply.github.com>
2025-04-28 17:54:01 +0100

    Update CreateNode.cs

--- Assets/Scripts/Nodes/CreateNode.cs
+++ Assets/Scripts/Nodes/CreateNode.cs
@@ -1,10 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CreateNode : MonoBehaviour
+public class CreateNode : MonoBehaviour, IPointerEnterHandler
 {
     [SerializeField] private NodeData NodeToCreate;
+    [SerializeField] private GameObject rangeIndicatorPrefab;
+    [SerializeField] private Color32 RangedIndicatorColor = new Color32();
+    private GameObject rangeIndicator;
+
+
+    private void Awake()
+    {
+        rangeIndicator = Instantiate(rangeIndicatorPrefab);
+        rangeIndicator.GetComponent<SpriteRenderer>().color = RangedIndicatorColor;
+        rangeIndicator.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        rangeIndicator.SetActive(true);
+        rangeIndicator.transform.localScale = new Vector2(1, 1) * NodeToCreate.connectionRange;
+        rangeIndicator.transform.position = ClickedPos.instance.LastClickedPosition;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rangeIndicator.SetActive(false);
+    }
 
     public void CreateNodeButton()
     {
