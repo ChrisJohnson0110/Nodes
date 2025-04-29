43a46172d4f67cfa8fbf66a1beba1f651182d287
chrisJohns0n <126682002+ChrisJohnson0110@users.noreply.github.com>
2025-04-29 13:11:35 +0100

    Update NodeManager.cs
    better node linking implementation

--- Assets/Scripts/Nodes/NodeManager.cs
+++ Assets/Scripts/Nodes/NodeManager.cs
@@ -7,6 +7,14 @@ public class NodeManager : MonoBehaviour
     public static NodeManager instance;
     [HideInInspector] public Dictionary<Vector2, Node> nodes { get; private set; } = new Dictionary<Vector2, Node>();
 
+    private Dictionary<NodeTypes, HashSet<NodeTypes>> connectionRules = new Dictionary<NodeTypes, HashSet<NodeTypes>> //rules for connections
+    {
+        { NodeTypes.transmitter, new HashSet<NodeTypes> { NodeTypes.Connection, NodeTypes.Reciever } },
+        { NodeTypes.Connection,  new HashSet<NodeTypes> { NodeTypes.transmitter, NodeTypes.Connection, NodeTypes.Reciever } },
+        { NodeTypes.Reciever,    new HashSet<NodeTypes> { NodeTypes.transmitter, NodeTypes.Connection } }
+    };
+
+
     private void Awake()
     {
         if (instance == null)
@@ -31,15 +39,14 @@ public class NodeManager : MonoBehaviour
             case NodeTypes.Connection: node = nodeObject.AddComponent<Connection>(); break;
             case NodeTypes.Reciever: node = nodeObject.AddComponent<Reciever>(); break;
         }
-
+        node.Initialize(a_nodeData);
         node.nodeObject = nodeObject;
         node.nodeObject.layer = LayerMask.NameToLayer("Node");
         node.nodeObject.transform.SetParent(this.gameObject.transform);
         node.nodeObject.transform.position = a_position;
 
-        //add connections
+        //establish connections between nearby nodes
         AddNodeConnections(node);
-
 
         //add to existing nodes
         nodes[a_position] = node;
@@ -51,12 +58,6 @@ public class NodeManager : MonoBehaviour
     public Node GetNode(GameObject a_gameObject)
     {
         return nodes[a_gameObject.transform.position];
-    }
-
-    private void UpdateNeatbyNodes(Node a_node)
-    {
-        //add all nearby nodes into node queue
-        //update all nearby nodes adding new node to the queue
     }
 
     private void AddNodeConnections(Node a_node)
@@ -71,49 +72,22 @@ public class NodeManager : MonoBehaviour
             }
             if (Vector2.Distance(a_node.transform.position, node.transform.position) < range)
             {
-                switch (a_node.nodeData.nodeType)
+                //does a connection rule apply for the nodedata type
+                if (connectionRules.TryGetValue(a_node.nodeData.nodeType, out var allowedTargets)) 
                 {
-                    case NodeTypes.transmitter: Transmitter(a_node, node); break;
-                    case NodeTypes.Connection: Connection(a_node, node); break;
-                    case NodeTypes.Reciever: Reciever(a_node, node); break;
+                    //if the node is allowed to connect
+                    if (allowedTargets.Contains(node.nodeData.nodeType))
+                    {
+                        FormConnection(a_node, node);
+                    }
+                    else
+                    {
+                        Debug.Log($"Cannot connect {a_node.nodeData.nodeType} to {node.nodeData.nodeType}");
+                    }
                 }
             }
         }
     }
-
-    //nodeOne is the tpye of node that has just been created
-    //nodeTwo is the node that has been checked and needs to be connected
-    //this is the transmitter function so has been called because nodeOne is a transmitter node
-    //depending on nodeTwo the objects that can be connected change
-    private void Transmitter(Node a_nodeOne, Node a_nodeTwo)
-    {
-        switch (a_nodeTwo.nodeData.nodeType)
-        {
-            case NodeTypes.transmitter: Debug.Log("cant connect to transmitter"); break;
-            case NodeTypes.Connection: FormConnection(a_nodeOne, a_nodeTwo); break;
-            case NodeTypes.Reciever: FormConnection(a_nodeOne, a_nodeTwo); break;
-        }
-    }
-    private void Connection(Node a_nodeOne, Node a_nodeTwo)
-    {
-        switch (a_nodeTwo.nodeData.nodeType)
-        {
-            case NodeTypes.transmitter: FormConnection(a_nodeOne, a_nodeTwo); break;
-            case NodeTypes.Connection: FormConnection(a_nodeOne, a_nodeTwo); break;
-            case NodeTypes.Reciever: FormConnection(a_nodeOne, a_nodeTwo); break;
-        }
-    }
-    private void Reciever(Node a_nodeOne, Node a_nodeTwo)
-    {
-        switch (a_nodeTwo.nodeData.nodeType)
-        {
-            case NodeTypes.transmitter: FormConnection(a_nodeOne, a_nodeTwo); break;
-            case NodeTypes.Connection: FormConnection(a_nodeOne, a_nodeTwo); break;
-            case NodeTypes.Reciever: Debug.Log("cant connect to reciever"); break;
-        }
-    }
-
-
 
     private void FormConnection(Node a_nodeOne, Node a_nodeTwo)
     {
